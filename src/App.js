70d02372f4d13c9f2ea8f35ca83372425627f475
artinsepanta <faribasalehi@hotmail.com>
2019-06-10 18:20:21 -0500

    save changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[],
+      text:'',
+      isClicked: false
+
+    };
+  }
+
+  onClick = () => { 
+    this.setState ({
+      isClicked:true,
+      todos:this.state.todos.concat(this.state.text),
+      text:''
+    })
+  }
+
+  onChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+
+    this.setState({
+      text:event.target.value
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+          {this.state.todos.map(function(value, index, array) {
+             return (
+               <li>
+                {value}
+               </li>
+             );
+          })}
+        <input value={this.state.text} onChange={this.onChange}/>
+        <button onClick={this.onClick}>Click Me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
